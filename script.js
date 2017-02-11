9426da0296c647cf07fb19aaa313f598921a5a05
Konstantin Kuchkov <konstantin.kuchkov@gmail.com>
2017-02-11 14:49:29 -0800

    Page now works if JavaScript is disabled. Without JS the lessons navigation is always fully expanded. Reorganized script.js.

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
-// "accordion" expansion for #lessons_nav
-function expand_sib() {
-    var lessons = this.nextElementSibling;
-    lessons.style.maxHeight =
-        lessons.style.maxHeight ? null : lessons.scrollHeight + "px";
-}
-
 function nav_exp_init() {
+    // "accordion" expansion for #lessons_nav
     var categories = document.getElementsByClassName("categories");
-    for (var i = 0; i < categories.length; i++)
-        categories[i].onclick = expand_sib;
-}
 
-// main width setters
-function set_lessons_width() {
-    document.getElementById("lessons_nav").style.width =
-        document.getElementById("top_nav")
-        .getElementsByTagName("li")[0].offsetWidth + "px";
-}
-
-function set_content_width() {
-    document.getElementById("lesson_content").style.width =
-        (document.getElementsByClassName("centered")[0].offsetWidth
-        - document.getElementById("lessons_nav").offsetWidth) + "px";
+    for(var i = 0; i < categories.length; i++) {
+        // set max-height to 0 here instead of in css so that
+        // lesson navigation is expanded by default if JavaScript is disabled
+        categories[i].nextElementSibling.style.maxHeight = "0px";
+        categories[i].onclick =
+            function() {
+                var lessons = this.nextElementSibling;
+                lessons.style.maxHeight =
+                    lessons.style.maxHeight == "0px" ?
+                    lessons.scrollHeight + "px" : "0px";
+            }
+    }
 }
 
 function set_main_width() {
-    set_lessons_width();
-    set_content_width();
+    var lessons_nav = document.getElementById("lessons_nav");
+
+    lessons_nav.style.width =
+        document.getElementById("top_nav")
+        .getElementsByTagName("li")[0].offsetWidth + "px";
+
+    document.getElementById("lesson_content").style.width =
+        (document.getElementsByClassName("centered")[0].offsetWidth
+            - lessons_nav.offsetWidth) + "px";
 }
