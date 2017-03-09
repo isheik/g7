4e4748c6168c690c0369009215bd06ce96b61d17
Keishi Asai <isheik808@gmail.com>
2017-03-08 19:18:46 -0800

    Add min5Chars and isValidEmail functions

--- script/atomic-validation-functions.js
+++ script/atomic-validation-functions.js
@@ -2,6 +2,10 @@ function isASCII(field){
 	return /^[\x00-\x7F]*$/.test(field);
 }
 
+function min5Chars(field) {
+    return field.length >= 5;
+}
+
 function max128Chars(field) {
     return field.length <= 128;
 }
@@ -20,4 +24,8 @@ function isNotBlank(field){
 
 function isValidUsername(field) {
     return !(/[^a-zA-Z\-\_0-9]/.test(field));
+}
+
+function isValidEmail(field) {
+    return /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})/.test(field) ;
 }
