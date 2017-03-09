fb6daa6efc71b01f6fdab46e22d81ea99fc8c977
pclundaahl <pc.lundaahl@gmail.com>
2017-03-08 19:11:14 -0800

    Fixed merge issue in warn-functions.js

--- script/warn-functions.js
+++ script/warn-functions.js
@@ -29,3 +29,34 @@ function warnEmail(field,output) {
 
     document.getElementById(output).innerHTML = "Email";
 }
+
+function warnREmail(field1,field2,output) {
+    field1 = document.getElementById(field1).value;
+    field2 = document.getElementById(field2).value;
+
+    if (field1 != field2)
+        document.getElementById(output).innerHTML = "E-mail addresses do not match";
+    else
+        document.getElementById(output).innerHTML = "";
+}
+
+// PASSWORD
+
+function warnPassword(field,output) {
+    // message = "test message";
+
+    // if (message != undefined)
+    //     document.getElementById(output).innerHTML = message;
+
+    document.getElementById(output).innerHTML = "Password";
+}
+
+function warnRePassword(field1,field2,output) {
+    field1 = document.getElementById(field1).value;
+    field2 = document.getElementById(field2).value;
+
+    if (field1 != field2)
+        document.getElementById(output).innerHTML = "Passwords do not match";
+    else
+        document.getElementById(output).innerHTML = "";
+}
