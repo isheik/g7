0b6b892c757555e12bace7eb4969bf6557e03910
Konstantin Kuchkov <konstantin.kuchkov@gmail.com>
2017-03-10 22:13:54 -0800

    Made templates for individual lessons.

---
+++ layout/screen-layout.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Windows 10 Shortcuts - Screen Layout</title>
+        <link rel="stylesheet" type="text/css" href="../style/base.css">
+        <link rel="stylesheet" type="text/css" href="../style/lessons.css">
+    </head>
+
+    <body>
+
+<?php require_once("../php_include/site_header.html") ?>
+
+        <main class="page_row">
+            <div id="content_wrapper" class="centered">
+
+<?php require_once("../php_include/lessons_nav.html") ?>
+
+                <article id="lesson_content">
+                    <h2>Screen Layout</h2>
+                </article>
+            </div>
+        </main>
+
+<?php require_once("../php_include/site_footer.html") ?>
+
+    </body>
+</html>
