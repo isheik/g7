d557d2513518a0da879e2e1fcad870f0f4ba7291
pclundaahl <pc.lundaahl@gmail.com>
2017-03-22 17:19:30 -0700

    Partial refactor of index.php and its css

--- index.php
+++ index.php
@@ -13,13 +13,13 @@
 
        <main class="page_row">
             <div id="content_wrapper" class="centered">
-                <article>
+                <article class="clearfix">
                     <div class="slide_show_images">
-                        <img class="mySlides" src="images/logo1.jpg" width="100%" height="100%" alt="d">
-                        <img class="mySlides" src="images/logo2.jpg" width="100%" height="100%" alt="d">
-                        <img class="mySlides" src="images/logo3.jpg" width="100%" height="100%" alt="d">
+                        <img class="mySlides" src="images/logo1.jpg" width="100%" height="auto" alt="d">
+                        <img class="mySlides" src="images/logo2.jpg" width="100%" height="auto" alt="d">
+                        <img class="mySlides" src="images/logo3.jpg" width="100%" height="auto" alt="d">
 
-                        <div class="button_allignment" style="width:100%">
+                        <div class="button_alignment" style="width:100%">
                             <div class="left_arrow" onclick="plusDivs(-1)">&#10094;</div>
                             <div class="right_arrow" onclick="plusDivs(1)">&#10095;</div>
                            
@@ -30,18 +30,17 @@
                     </div>
                 
                     <div>
-                        <h2>Do not wait for a perfect moment. Take the moment and make it perfect.</h2>
                         <p class="p">Lorem ipsum dolor sit amet, Suspendisse dignissim Lorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dignissim Lorem ipsum dolor sit amet, con</p>
                     </div>
                     
                     
-                    <div class="featured-content">
+                    <div class="featured_content">
                         <img src="images/abcd.jpg" height="200" width="400" alt="p">
                         <h3>About us</h3>
                         <p>Find out about our organization, mission, our methods, and the results of our decades of advocacy.</p>
                     </div>
                     
-                    <div class="featured-content">
+                    <div class="featured_content">
                         <img src="images/abcd.jpg" height="200" width="400" alt="p">
                         <h3>Take action</h3>
                         <p>Ready to take the first step? You can start with beginner level and become a expert</p>
