b78f51a006686ba02411960cfff64c437478c1f6
Patrick Charles-Lundaahl <pc.lundaahl@gmail.com>
2017-03-29 18:18:41 -0700

    Added new php support files (add_topic.php and add_response.php) in prep for db integration

--- add_topic.php
+++ add_topic.php
@@ -1,66 +1,25 @@
 <?php
-    // Temporarily using sample database. Need to change config.php later.
-    // Start session
-    session_start();
-
-    // Load config file if it was not.
-    require_once('config.php'); 
+    require_once('auth.php');
+    require_once('config.php');
 
     // Connect to server and select database.
-    ($GLOBALS["___mysqli_ston"] = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD)) or die("cannot connect");
-    ((bool)mysqli_query($GLOBALS["___mysqli_ston"], "USE " . constant('DB_DATABASE'))) or die("cannot select DB");
+    ($GLOBALS["___mysqli_ston"] = mysqli_connect(DB_HOST,  DB_USER,  DB_PASSWORD))or die("cannot connect");
+    ((bool)mysqli_query($GLOBALS["___mysqli_ston"], "USE " . constant('DB_DATABASE')))or die("cannot select DB");
+    $tbl_name="topic"; // Table name
+
+    // get data that sent from form
+    $topic=$_POST['post_title'];
+    $detail=$_POST['post_contents'];
+    $member_id=$_SESSION['SESS_MEMBER_ID'];
+    $datetime=date("d/m/y h:i:s"); //create date time
+
+    $sql="INSERT INTO $tbl_name(topic, detail, member_id, datetime) VALUES ('$topic', '$detail', '$member_id', '$datetime')";
+    $result=mysqli_query($GLOBALS["___mysqli_ston"], $sql);
+
+    if($result)
+        header("location: forum.php");
+    else {
+        echo "ERROR";
+    }
+    ((is_null($___mysqli_res = mysqli_close($GLOBALS["___mysqli_ston"]))) ? false : $___mysqli_res);
 ?>
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title>Windows 10 Shortcuts</title>
-        <link rel="stylesheet" type="text/css" href="style/base.css">
-        <link rel="stylesheet" type="text/css" href="style/add_topic.css">
-        <script src="script/warn-functions.js"></script>
-    </head>
-
-    <body>
-
-<?php require_once("./php_include/site_header.php") ?>
-
-        <main class="page_row">
-            <div id="content_wrapper" class="centered">
-                <article id="forum_add_topic">
-                    <h2>Forum</h2>
-                    <p><a href="./forum.php">Return to forum top page</a></p>
-                    <form method="post" action="http://webdevbasics.net/scripts/demo.php"
-                        onsubmit="return warnPost('forum_post_title', 'forum_post_text', 
-                        'forum_post_title_feedback', 'forum_post_text_feedback')">
-                        <table id="forum_post_table" class="layout_table">
-
-                            <tr>
-                                <th><label for="forum_post_title">Title:</label></th>
-                                <td><input required maxlength="128" type="text" id="forum_post_title" name="title"
-                                    oninput="warnTitle('forum_post_title','forum_post_title_feedback')">
-                                    <label class="error" id="forum_post_title_feedback"></label>
-                                </td>
-                            </tr>
-
-                            <tr id="forum_post_form">
-                                <th><label for="forum_post_text">Content:</label></th>
-                                <td><textarea required id="forum_post_text" name="input"
-                                    oninput="warnContent('forum_post_text','forum_post_text_feedback')"></textarea>
-                                    <label class="error" id="forum_post_text_feedback"></label>
-                                    <button type="submit" name="post">Post</button>
-                                </td>
-                            </tr>
-
-                        </table>
-                    </form>
-                </article>
-            </div>
-        </main>
-
-<?php require_once("./php_include/site_footer.php") ?>
-
-    </body>
-</html>
