09ab636c265aa3dbbdc80f231820160156b55c4f
Konstantin Kuchkov <konstantin.kuchkov@gmail.com>
2017-04-01 21:21:43 -0700

    Replaced <?php echo ... ?> with shorthand <?= ... ?>

--- php_include/site_header.php
+++ php_include/site_header.php
@@ -13,7 +13,7 @@
 ?>
         <header class="page_row">
             <div class="centered">
-                <a href="<?php echo $add_to_path ?>index.php"><img src="<?php echo $add_to_path ?>images/logo_header.png" id="header_logo" alt="logo" width="120" height="108"></a>
+                <a href="<?= $add_to_path ?>index.php"><img src="<?= $add_to_path ?>images/logo_header.png" id="header_logo" alt="logo" width="120" height="108"></a>
                 <h1 id="header_title">Windows 10 Shortcuts</h1>
                 <div id="auth">
                     <?php 
@@ -31,12 +31,12 @@
         <nav id="top_nav" class="site_nav page_row">
             <div class="centered">
                 <ul>
-                    <li><a href="<?php echo $add_to_path ?>index.php">Home</a></li>
-                    <li><a href="<?php echo $add_to_path ?>lessons-main.php">Lessons</a></li>
-                    <li><a href="<?php echo $add_to_path ?>catalog.php">Catalog</a></li>
-                    <li><a href="<?php echo $add_to_path ?>forum.php">Forum</a></li>
-                    <li><a href="<?php echo $add_to_path ?>contact.php">Contact</a></li>
-                    <li><a href="<?php echo $add_to_path ?>about.php">About</a></li>
+                    <li><a href="<?= $add_to_path ?>index.php">Home</a></li>
+                    <li><a href="<?= $add_to_path ?>lessons-main.php">Lessons</a></li>
+                    <li><a href="<?= $add_to_path ?>catalog.php">Catalog</a></li>
+                    <li><a href="<?= $add_to_path ?>forum.php">Forum</a></li>
+                    <li><a href="<?= $add_to_path ?>contact.php">Contact</a></li>
+                    <li><a href="<?= $add_to_path ?>about.php">About</a></li>
                 </ul>
             </div>
         </nav>
