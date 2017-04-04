942a5c7bcd003690d619d0c33ab45ed12f343584
Patrick Charles-Lundaahl <pc.lundaahl@gmail.com>
2017-04-03 17:54:28 -0700

    Added content for window-switching.php

--- layout/window-switching.php
+++ layout/window-switching.php
@@ -28,7 +28,13 @@
                     <p>An alternative to this is <kbd>alt + esc</kbd>. This performs the same functionality, but without the pop-up display.</p>
 
                     <h3>Tiling</h3>
-                    <p><kbd>Win + tab</kbd> 
+                    <p><kbd>Win + tab</kbd> is a third method for switching apps. However, instead of stepping through them, it neatly tiles them and lets you pick one with your mouse. A neat extra feature is that this will also show any other desktops you have open. If that's new to you, don't worry about that right now. We'll get to that in a few lessons.</p>
+
+                    <h3>Cycling Tabs</h3>
+                    <p>Just like switching through apps, you can also cycle through any tabs you have open in your current app. This makes sense in browsers, as well as any other app that features multiple tabs. To cycle forward, use <kbd>ctrl + tab</kbd>, and to cycle backwards use <kbd>ctrl + shift + tab</kbd>.</p>
+
+                    <h3>Closing</h3>
+                    <p>Although probably not as useful as the rest of these, <kbd>alt + f4</kbd> is a Windows staple. Pressing this keyboard shortcut will close your current app. This works just like pressing the close button on the app's taskbar, so you'll be asked to save any open files, just like normal.</p>
                 </article>
             </div>
         </main>
