81b4275797a2a5dac413fd0da1b0b8d2683180fb
Patrick Charles-Lundaahl <pc.lundaahl@gmail.com>
2017-04-03 19:04:05 -0700

    Added content to about page (still needs to be filled out for each individual member.

--- about.php
+++ about.php
@@ -15,8 +15,22 @@
             <div id="content_wrapper" class="centered">
                 <article id="about">
                     <h2>About</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dignissim mauris et pretium molestie. Cras ac felis et orci imperdiet lobortis ut sed arcu. Mauris mollis sed arcu vitae tristique. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Curabitur vulputate quam at sem blandit, sit amet gravida orci convallis. Sed pellentesque sollicitudin tellus, eget rutrum urna porttitor ut. Sed ullamcorper, mauris vitae lacinia varius, lacus lorem porttitor elit, non finibus tortor odio in dui.</p>
-                    <p>Proin hendrerit lectus in enim tincidunt, ac tincidunt ipsum ullamcorper. Aliquam et porta turpis, imperdiet placerat eros. Fusce pretium, justo ac sagittis hendrerit, odio quam dignissim est, porta facilisis enim dolor ac ex. Vivamus ullamcorper elementum dui. Donec et maximus urna, vel feugiat mi. Nam id ipsum ipsum. Sed non erat purus. Sed dictum imperdiet facilisis. Maecenas dapibus quam in ante malesuada, id varius augue auctor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nullam id eros quis tellus ornare luctus.</p>
+                    <p>Welcome to Windows 10 Shortcuts, a project by five first-year students in the Computer Systems Technology program at British Columbia Institute of Technology (BCIT). For most of us, this has been our first introduction to many different web technologies, including HTML5, CSS, JavaScript, PHP, and SQL. We hope it provides you with something genuinely useful.</p>
+
+                    <h3>Alireza Satteri</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit eius earum, eum inventore veniam cumque accusantium saepe commodi, assumenda dolorem placeat rerum dolores? Sed ea eaque, consequatur id earum eveniet repudiandae voluptates nesciunt dolorem, totam laborum numquam explicabo minus. Unde molestiae, ipsa deserunt dignissimos esse pariatur, error eos itaque similique natus cumque praesentium, iure libero labore officiis aliquid ab sit!</p>
+
+                    <h3>Karanveer Singh</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus eligendi error, facilis laudantium officiis. Sequi illum deserunt eius explicabo, veniam molestias consequuntur voluptate architecto vel, consequatur aliquam repellendus eligendi itaque cupiditate dicta sed omnis officiis excepturi. Rerum repudiandae molestiae, earum dignissimos accusantium cum accusamus ab architecto recusandae consectetur atque dolore cumque similique fuga omnis, iste deleniti. Ipsum minus culpa distinctio.</p>
+
+                    <h3>Keishi Asai</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus eligendi error, facilis laudantium officiis. Sequi illum deserunt eius explicabo, veniam molestias consequuntur voluptate architecto vel, consequatur aliquam repellendus eligendi itaque cupiditate dicta sed omnis officiis excepturi. Rerum repudiandae molestiae, earum dignissimos accusantium cum accusamus ab architecto recusandae consectetur atque dolore cumque similique fuga omnis, iste deleniti. Ipsum minus culpa distinctio.</p>
+
+                    <h3>Konstantin Kuchkov</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus eligendi error, facilis laudantium officiis. Sequi illum deserunt eius explicabo, veniam molestias consequuntur voluptate architecto vel, consequatur aliquam repellendus eligendi itaque cupiditate dicta sed omnis officiis excepturi. Rerum repudiandae molestiae, earum dignissimos accusantium cum accusamus ab architecto recusandae consectetur atque dolore cumque similique fuga omnis, iste deleniti. Ipsum minus culpa distinctio.</p>
+
+                    <h3>Patrick Charles-Lundaahl</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus eligendi error, facilis laudantium officiis. Sequi illum deserunt eius explicabo, veniam molestias consequuntur voluptate architecto vel, consequatur aliquam repellendus eligendi itaque cupiditate dicta sed omnis officiis excepturi. Rerum repudiandae molestiae, earum dignissimos accusantium cum accusamus ab architecto recusandae consectetur atque dolore cumque similique fuga omnis, iste deleniti. Ipsum minus culpa distinctio.</p>
                 </article>
             </div>
         </main>
