f1774b1cf85b6e4dbb82a354ead02deda63624b5
Konstantin Kuchkov <konstantin.kuchkov@gmail.com>
2017-04-10 14:47:07 -0700

    Slideshow timer now resets interval when slide is selected manually.

--- script/home.js
+++ script/home.js
@@ -1,27 +1,36 @@
 var slideIndex = 1;
-showDivs(slideIndex);
-setInterval(function(){plusDivs(1)}, 7000);
+var slideInterval = 7000;
+var timer;
 
 function plusDivs(n) {
-  showDivs(slideIndex += n);
+    showDivs(slideIndex += n);
 }
 
 function currentDiv(n) {
-  showDivs(slideIndex = n);
+    showDivs(slideIndex = n);
+}
+
+function startTimer() {
+    timer = setTimeout(function(){plusDivs(1)}, slideInterval);
 }
 
 function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("button_configuration");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-     dots[i].className = dots[i].className.replace(" white", "");
-  }
-  x[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " white";
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("button_configuration");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" white", "");
+    }
+    x[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " white";
+
+    clearTimeout(timer);
+    startTimer();
 }
+
+showDivs(slideIndex);
